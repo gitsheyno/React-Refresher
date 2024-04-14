b285c24fadc3630cf44591dc0ffd845e24025866
shayan.heidary <shayan.heidary@knime.com>
2024-04-14 13:29:51 +0200

    added react query

--- src/Details.jsx
+++ src/Details.jsx
@@ -1,5 +1,34 @@
 import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query";
+import fetchPet from "./fetchPet";
+
 export const Details = () => {
   const { id } = useParams();
-  return <div>this is the id : {id}</div>;
+  const results = useQuery(["details", id], fetchPet);
+
+  //isLoading for first load
+  //isFetching for refetching
+
+  if (results.isLoading) {
+    return (
+      <div className="loading-pane">
+        <h2 className="loader">🌀</h2>
+      </div>
+    );
+  }
+
+  const pet = results.data.pets[0];
+
+  return (
+    <div className="details">
+      <div>
+        <h1>{pet.name}</h1>
+        <h2>
+          {pet.animal} - {pet.breed} - {pet.city} - {pet.state}
+          <button>Adopt {pet.name}</button>
+          {pet.description}
+        </h2>
+      </div>
+    </div>
+  );
 };
---
+++ src/fetchBreed.js
@@ -0,0 +1,1 @@
+const fetchBreed = async({ queryKey });
