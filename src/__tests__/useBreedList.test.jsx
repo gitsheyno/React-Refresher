c8176223ede6326a34da36edbaa820aa4ed7024c
shayan.heidary <shayan.heidary@knime.com>
2024-04-28 13:06:59 +0200

    added mock test

--- src/__tests__/useBreedList.test.jsx
+++ src/__tests__/useBreedList.test.jsx
@@ -1,7 +1,9 @@
 import { test, expect } from "vitest";
-import { renderHook } from "@testing-library/react";
+import { renderHook, waitFor } from "@testing-library/react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import useBreedList from "../useBreedList";
+import AdoptedPetContext from "../AdoptPetContext";
+import { useContext, useState } from "react";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -25,4 +27,45 @@ test("gives an empty list with no animal", async () => {
 
   expect(breedList).toHaveLength(0);
   expect(status).toBe("loading");
+});
+
+test("gives back breeds with an animal", async () => {
+  const breeds = [
+    "Havanese",
+    "Bichon Frise",
+    "Poodle",
+    "Maltese",
+    "Golden Retriever",
+    "Labrador",
+    "Husky",
+  ];
+
+  fetch.mockResponseOnce(
+    JSON.stringify({
+      animal: "dog",
+      breeds,
+    })
+  );
+
+  const { result } = renderHook(() => useBreedList("dog"), {
+    wrapper: ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    ),
+  });
+
+  await waitFor(() => expect(result.current[1]).toBe("success"));
+
+  expect(result.current[0]).toEqual(breeds);
+});
+
+test("context", async () => {
+  const results = renderHook(() => useContext(AdoptedPetContext), {
+    wrapper: ({ children }) => (
+      <AdoptedPetContext.Provider value={{ name: "shayan" }}>
+        {children}
+      </AdoptedPetContext.Provider>
+    ),
+  });
+
+  expect(results.result.current).toEqual({ name: "shayan" });
 });
